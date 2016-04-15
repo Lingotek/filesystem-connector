e414c2688ab663db364e8d3e322d14479f80a547
Joshua Mathias <joshuamonkey@gmail.com>
2016-04-15 10:46:14 -0600

    Exception test

--- ltk/commands.py
+++ ltk/commands.py
@@ -148,6 +148,7 @@ def add(file_names, locale, **kwargs):
         init_logger(action.path)
         action.add_action(locale, file_names, **kwargs)
     except (UninitializedError, RequestFailedError, ResourceNotFound, AlreadyExistsError) as e:
+        print("Exception test")
         print_log(e)
         logger.error(e)
         return
@@ -170,7 +171,7 @@ def push():
 @click.option('-n', '--doc_name', help='the name of the document, specify for one document')
 @click.option('-d', '--delete', 'to_delete', flag_value=True, help='deletes a specified target locale')
 @click.option('--due_date', help='the due date of the translation')
-@click.option('-w', '--workflow', help='the workflow of the translation')
+@click.option('-w', '--workflow', help='the workflow of the translation (do "ltk list -w" to see available workflows)')
 @click.argument('locales', required=True, nargs=-1)  # can have unlimited number of locales
 def request(doc_name, locales, to_delete, due_date, workflow):
     """ add targets to document(s) to start translation, defaults to entire project """
