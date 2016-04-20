defb067b7b50aba0da6d4edd7e17d471d946d44f
Jessie Young <jyoung@lingotek.com>
2016-04-20 15:05:42 -0600

    modify setup.py so that it automatically converts readme to correc format for pypi

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 from setuptools import setup, find_packages
 from ltk import __version__
 
+try:
+    import pypandoc
+
+    long_description = pypandoc.convert('README.md', 'rst')
+    long_description = long_description.replace("\r", "")
+except (IOError, ImportError, OSError):
+    long_description = open('README.md').read()
+
 setup(
     name='ltk',
     version=__version__,
     url='https://github.com/Lingotek/client',
     description='Command line tool that connects your content to the Lingotek Translation Network',
+    long_description=long_description,
     author='Lingotek',
     author_email='integrations@lingotek.com',
     license='MIT',
