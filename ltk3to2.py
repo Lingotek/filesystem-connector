12ad32c2c8ca550881a5b5b64c733fbb02e5939b
Joshua Mathias <joshuamonkey@gmail.com>
2016-08-02 11:27:15 -0600

    Include end comment lines in Python 2 files

--- ltk3to2.py
+++ ltk3to2.py
@@ -54,11 +54,13 @@ for fpath in files3:
             elif is_python2:
                 if '# End Python 2' in line:
                     is_python2 = False
-                line = line.replace('# ','')
+                else:
+                    line = line.replace('# ','')
             elif '# Python 3' in line:
                 is_python3 = True
             elif is_python3:
                 if '# End Python 3' in line:
                     is_python3 = False
-                line = '# '+str(line)
+                else:
+                    line = '# '+str(line)
             f.write(line)
