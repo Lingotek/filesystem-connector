1f74296b63fa0d1db7285492ec4698eb868dafef
Spencer Olsen <spencer.p.olsen@gmail.com>
2017-01-18 13:20:06 -0700

    Fix merge conflict in init file

--- python3/ltk/actions/__init__.py
+++ python3/ltk/actions/__init__.py
@@ -3,16 +3,7 @@ __all__ = [ "action",
             "request_action",
             "add_action",
             "push_action",
-<<<<<<< HEAD
-<<<<<<< HEAD
             "download_action",
-            "move_action"
-=======
             "pull_action",
             "clean_action"
->>>>>>> int-1791
-=======
-            "pull_action",
-            "download_action"
->>>>>>> int-1787
           ]
