92c33b125f195007974d5ff9e2fc7bc4c3d670e7
Austin A <aahlstrom@lingotek.com>
2017-01-19 14:48:34 -0700

    Fixing merge conflicts

--- python2/ltk/actions/__init__.py
+++ python2/ltk/actions/__init__.py
@@ -1,8 +1,13 @@
 __all__ = [ "action",
             "action_facade",
-            "request_action",
             "add_action",
-            "push_action",
+            "clean_action",
             "download_action",
-            "rm_action"
+            "filters_action",
+            "pull_action",
+            "push_action",
+            "request_action",
+            "rm_action",
+            "status_action",
+            #"move_action",
           ]
