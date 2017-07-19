ee459aad27e28a5591f36d15ed635e84ec6e98be
Spencer Olsen <spencer.p.olsen@gmail.com>
2017-07-19 13:56:07 -0600

    Updated version tags to 1.2.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ except (IOError, ImportError, OSError):
 
 setup(
     name='ltk',
-    version='1.2.0',
+    version='1.2.1',
     url='https://github.com/Lingotek/filesystem-connector',
     description='The Lingotek Filesystem Connector (ltk) links your files and folders to the Translation Network™',
     long_description=long_description,
