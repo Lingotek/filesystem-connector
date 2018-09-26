cde67953f526cd7d4a0c4f195112186bab211a10
Edward A. Portela Portela <EPortela@lingotek.com>
2018-09-26 15:09:39 -0600

    Added an extra blank line before set up option for finalized filed download

--- python2/ltk/actions/init_action.py
+++ python2/ltk/actions/init_action.py
@@ -272,7 +272,7 @@ class InitAction():
             config_parser.set('main', 'clone_option', 'off')
 
         # Toggle finalized file download option
-        print("--------------------------------")
+        print("\n--------------------------------")
         print("DOWNLOAD FINALIZED FILE:")
         print("Toggle finalized file download option 'on' or 'off'. Turning this option on downloads the finalized file instaed of the raw translation. \
               A finalized file is typically a file that has undergone some sort of post editing like Desktop Publishing after the translation has completed.")
