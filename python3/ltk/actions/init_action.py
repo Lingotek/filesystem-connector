9c1c526210c731242133d8421671ede591b4ec2e
Edward A. Portela Portela <EPortela@lingotek.com>
2018-09-27 16:49:21 -0600

    Fixed missing newline in code

--- python3/ltk/actions/init_action.py
+++ python3/ltk/actions/init_action.py
@@ -266,7 +266,7 @@ class InitAction():
         print("DOWNLOAD FINALIZED FILE:")
         print("Toggle finalized file download option 'on' or 'off'. Turning this option on downloads the finalized file instaed of the raw translation. " +
               "A finalized file is typically a file that has undergone some sort of post editing like Desktop Publishing after the translation has completed.")
-              self.finalized_file = self.set_finalized_file_option()
+        self.finalized_file = self.set_finalized_file_option()
         config_parser.set('main', 'finalized_file', self.finalized_file)
 
     def check_global(self, host):
