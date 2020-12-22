c8c48a2f9a398d44a2e2a236ab893d760ef8ece7
Hector Lopez <hlopez@lingotek.com>
2020-12-22 15:45:11 -0700

    Ran ltk3to2 script

--- python2/ltk/actions/action.py
+++ python2/ltk/actions/action.py
@@ -137,7 +137,8 @@ class Action:
                 locale = self.locale
             else:
                 locale = kwargs['locale']
-
+            if not 'translation_locale_code' in kwargs and len(self.watch_locales) > 0:
+                kwargs['translation_locale_code'] = self.watch_locales
             # add document to Lingotek cloud
             response = self.api.add_document(locale, file_name, self.project_id, self.append_location(title, file_name), doc_metadata, **kwargs)
             if response.status_code == 402:
@@ -549,6 +550,11 @@ class Action:
         try:
             relative_path = self.norm_path(file_name)
             entry = self.doc_manager.get_doc_by_prop('file_name', relative_path)
+            kwargs['project_id'] = self.project_id
+            entry_locales = set(entry['locales']) if 'locales' in entry and entry['locales'] else set()
+            # if doc has no locales, or all watch locales, send them with translation_locale_code so that the project default workflow is applied to all
+            if len(entry_locales) > 0 and self.watch_locales.issuperset(entry_locales):
+                kwargs['translation_locale_code'] = entry_locales
             try:
                 document_id = entry['id']
             except TypeError as e:
@@ -558,7 +564,7 @@ class Action:
             if title:
                 response, previous_doc_id = self.locked_doc_response_manager(self.api.document_update(document_id, file_name, title=title, **kwargs), document_id, file_name, title=title, **kwargs)
             else:
-                response, previous_doc_id = self.locked_doc_response_manager(self.api.document_update(document_id, file_name), document_id, file_name, **kwargs)
+                response, previous_doc_id = self.locked_doc_response_manager(self.api.document_update(document_id, file_name, title=None, **kwargs), document_id, file_name, **kwargs)
 
             if response.status_code == 410:
                 target_locales = entry['locales']
