9b938bb751ff8cedb53ec662f0187920ca392493
Nischal Paudyal <nischal@Nischals-MacBook-Air.local>
2021-08-12 11:18:44 -0500

    Removed the ability to change the project default workflow

--- python3/ltk/actions/config_action.py
+++ python3/ltk/actions/config_action.py
@@ -411,9 +411,6 @@ class ConfigAction(Action):
                 self.watch_locales = target_locales
 
     def set_workflow_id(self, workflow_id):
-        response = self.api.patch_project(self.project_id, workflow_id)
-        if response.status_code != 204:
-            raise_error(response.json(), 'Something went wrong trying to update workflow_id of project')
         self.workflow_id = workflow_id
         log_info = 'Project default workflow has been updated to {0}'.format(workflow_id)
         self.update_config_file('workflow_id', workflow_id, self.conf_parser, self.config_file_name, log_info)
